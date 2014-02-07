7bfd760af3d4e5c6ce3a60dd2e1db97263d6fcf0
Christopher Gwizdala <cgwizdala@sapient.com>
2014-02-07 15:59:03 -0800

    added TileCarousel example

---
+++ research/TileCarousel/Carousel/syntaxhighlighter/header-include.php
@@ -0,0 +1,88 @@
+<link rel="stylesheet" href="../syntaxhighlighter/styles/shCore.css" type="text/css" media="screen" title="no title" charset="utf-8">
+<link rel="stylesheet" href="../syntaxhighlighter/styles/shThemeDefault.css" type="text/css">
+<style type="text/css" media="screen">
+	body {
+		background:url(../../images/checkout/background-body.gif) repeat-x 0 0;
+	}
+	#Core {
+		background:url(../../images/checkout/background-core.gif) no-repeat;
+	}
+	h1 {
+		color:#E0E0E0;
+		font-size:84px;
+		font-weight:bold;
+		letter-spacing:-0.04em;
+		padding:45px 0 0 205px;
+		margin-bottom:-200px;
+	}
+	.section-intro-title {
+		color:#000000;
+		font-size:28px;
+		font-weight:bold;
+		margin-bottom:20px;
+	}
+	.section-intro {
+		
+	}
+	.section-intro p {
+		margin-bottom:15px;
+	}
+	.section-intro ul {
+		padding:0 0 0 14px;
+		margin-bottom:15px;
+	}
+	.section-intro ul li {
+		list-style:disc;
+		line-height:19px;
+	}
+	.example-section,
+	.demo {
+		padding:0 0 0 20px;
+	}
+	hr.section-divider {
+		height:1px;
+		border:none;
+		margin:0 0 30px;
+		border-bottom:1px dotted #999;
+	}
+	code {
+		font-weight:bold;
+		font-size:14px;
+		font-family:monospace;
+	}
+	.section-intro {
+		margin-bottom:20px;
+	}
+	.section-intro ul {
+		margin-bottom:20px;
+	}
+	.example-title {
+		color:#000000;
+		font-size:24px;
+		font-weight:bold;
+		margin-bottom:20px;
+	}
+	ul.example-desc {
+		padding:0 0 0 14px;
+		margin-bottom:20px;
+	}
+	ul.example-desc li {
+		list-style:disc;
+		line-height:19px;
+		padding:0 0 10px 0;
+	}
+	ul.example-desc li code:first-child {
+		padding:2px;
+		background:yellow;
+	}
+</style>
+<script src="../syntaxhighlighter/src/shCore.js" type="text/javascript" charset="utf-8"></script>
+<script src="../syntaxhighlighter/scripts/shBrushCss.js" type="text/javascript" charset="utf-8"></script>
+<script src="../syntaxhighlighter/scripts/shBrushJScript.js" type="text/javascript" charset="utf-8"></script>
+<script src="../syntaxhighlighter/scripts/shBrushPlain.js" type="text/javascript" charset="utf-8"></script>
+<script src="../syntaxhighlighter/scripts/shBrushXml.js" type="text/javascript" charset="utf-8"></script>
+<script src="../syntaxhighlighter/scripts/shBrushPhp.js" type="text/javascript" charset="utf-8"></script>
+<script type="text/javascript" charset="utf-8">
+	SyntaxHighlighter.config.clipboardSwf = '../syntaxhighlighter/scripts/clipboard.swf';
+	SyntaxHighlighter.all();
+</script>
