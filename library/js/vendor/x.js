66e21a5e170fdfe7b10934ccc8151f19132b9345
J Hausmann <jhausmann@sapient.com>
2015-03-14 22:26:25 -0700

    Optimized x.js code

--- library/js/vendor/x.js
+++ library/js/vendor/x.js
@@ -1,42 +1,3 @@
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/bind
-if ( !Function.prototype.bind ) {
-    Function.prototype.bind = function ( oThis ) {
-        if ( typeof this !== 'function' ) {
-            // closest thing possible to the ECMAScript 5 internal IsCallable function
-            throw new TypeError( 'Function.prototype.bind - what is trying to be bound is not callable' );
-        }
-
-        var aArgs = Array.prototype.slice.call( arguments, 1 ),
-            fToBind = this,
-            fNOP = function () {},
-            fBound = function () {
-              return fToBind.apply( this instanceof fNOP && oThis
-                                     ? this
-                                     : oThis,
-                                   aArgs.concat( Array.prototype.slice.call( arguments ) ) );
-            };
-
-        fNOP.prototype = this.prototype;
-        fBound.prototype = new fNOP();
-
-        return fBound;
-    };
-}
-
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/create
-if ( typeof Object.create !== 'function' ) {
-    ( function () {
-        var F = function () {};
-        Object.create = function ( o ) {
-            if ( arguments.length > 1 ) { throw Error( 'Second argument not supported' ); }
-            if ( o === null ) { throw Error( 'Cannot set a null [[Prototype]]' ); }
-            if ( typeof o !== 'object' ) { throw TypeError( 'Argument must be an object' ); }
-            F.prototype = o;
-            return new Fx;
-        };
-    })();
-}
-
 define(
 
     [],
@@ -48,11 +9,11 @@ define(
         // The X library's constructor
         function xApi( component ) {
 
-            var xObj = {};
-
-            xObj.channels = {};
-            xObj.tokenUid = -1;
-            xObj.component = component;
+            var xObj = {
+                channels: {},
+                tokenUid: -1,
+                component: component
+            };
 
             xObj.getState = function( key ) {
                 return component.state[ key ];
@@ -166,45 +127,41 @@ define(
             return xObj;
         }
 
-        xApi.define = function( namespace, inter ) {
+        xApi.define = function( namespace, spec ) {
 
             var plugins = {};
 
             // Component constructor
             var constructor = function( config ) {
 
-                var compInterface = {};
+                // Create interface for new instance
+                var compInterface = {
+                    state: {},
+                    ns: namespace,
+                    setupPlugins: function setupPlugins() {
 
-                compInterface.state = {};
-                compInterface.ns = namespace;
+                        for ( var member in plugins ) {
 
-                // Provide an new instance of X
-                // Pass in the component
-                compInterface.x = xApi( compInterface );
-                compInterface.x.ns = namespace;
-                
-                /*
-                 *  Provide interface for instance
-                 */
-                inter.setupPlugins = function setupPlugins() {
+                            if ( plugins.hasOwnProperty( member ) ) {
 
-                    for ( var member in plugins ) {
+                                if ( !( member in compInterface.options ) ) continue;
 
-                        if ( plugins.hasOwnProperty( member ) ) {
-
-                            if ( !( member in compInterface.options ) ) continue;
-
-                            plugins[ member ]( compInterface.x, compInterface.options[ member ] );
+                                plugins[ member ]( compInterface.x, compInterface.options[ member ] );
+                            }
                         }
                     }
                 };
+                    
+                // Provide an new instance of X
+                compInterface.x = xApi( compInterface );
+                compInterface.x.ns = namespace;
 
-                // Copy provided component core object & properties defined above into instance interface
-                for ( var member in inter ) {
+                // Copy spec into newly created instance interface
+                for ( var prop in spec ) {
 
-                    if ( inter.hasOwnProperty( member ) ) {
+                    if ( spec.hasOwnProperty( prop ) ) {
 
-                        compInterface[ member ] = inter[ member ];
+                        compInterface[ prop ] = spec[ prop ];
                     }
                 }
 
